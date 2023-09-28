a397768badd37cde91df699c14da6e5fd7ae3384
Psych <abdullahbinrafique945@gmail.com>
2023-09-28 17:48:36 +0500

    practice of loops

--- Day_01/practice.js
+++ Day_01/practice.js
@@ -86,3 +86,57 @@ for (let roll in obj) {
 for (let a of "abdullah") {
   console.log(a);
 }
+
+//<----------#Functions in JS---------->
+//lets make a function
+const avg2num = (n1, n2) => {
+  return (n1 + n2) / 2;
+};
+
+let num1 = 10,
+  num2 = 20;
+let avg = avg2num(num1, num2);
+console.log("average of two nums is : " + avg);
+
+//<----------#practice---------->
+// Q:1 print marks of object
+console.log("\n");
+const marks = {
+  abdullah: 47,
+  huri: 45,
+  talha_tariq: 44,
+  munam: 49,
+};
+for (let mark in marks) {
+  console.log("marks of " + mark + " : " + marks[mark]);
+}
+
+console.log("\n");
+for (let i = 0; i < Object.keys(marks).length; i++) {
+  console.log(
+    "Marks of " + Object.keys(marks)[i] + " : " + marks[Object.keys(marks)[i]]
+  );
+}
+
+// Problem No 3
+// let cn = 43;
+// let i;
+// while (i != cn) {
+//   i = prompt("Enter a number")
+//   console.log("Please Try Again")
+// }
+// console.log("You have entered a correct number")
+
+//problem no 4
+//fucntion to find mean of 5 numbers
+const mean5num = (n1, n2, n3, n4, n5) => {
+  return (n1 + n2 + n3 + n4 + n5) / 5;
+};
+
+let n1 = 10,
+  n2 = 20,
+  n3 = 30,
+  n4 = 40,
+  n5 = 50;
+let mean = mean5num(n1, n2, n3, n4, n5);
+console.log("\nMean of five numbers is : " + mean);
