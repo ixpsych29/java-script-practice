bb588a8273df26d14b88c9b2f73af5ecbd575feb
Psych <abdullahbinrafique945@gmail.com>
2023-10-02 14:36:29 +0500

    array advanced methods done practice done

---
+++ Day_02/quiz.js
@@ -0,0 +1,40 @@
+// array quiz problems
+// Q1: add values from users
+// let arr = [1, 2, 3, 4, 88];
+// let a = prompt("Enter the number");
+// a = Number(a);
+// arr.push(a);
+// console.log(arr);
+// this will add the value from the user
+
+// Q2: add untill when 0 is added
+// let arr = [1, 2, 3, 4, 88];
+// let a;
+// do {
+//   a = prompt("Enter the number");
+//   a = Number(a);
+//   arr.push(a);
+// } while (a != 0);
+
+// console.log(arr);
+
+// Q3: use filter method to filter numbers /10;
+// let arr = [3, 10, 40, 8, 50, 9, 80, 77, 990];
+// let newArr = arr.filter((el) => {
+//   return el % 10 == 0;
+// });
+// console.log(newArr);
+
+// Q4: array of square of given numbers
+// let arr = [1, 2, 3, 5, 6, 7, 9, 0];
+// let sqrdArr = arr.map((el) => {
+//   return el * el;
+// });
+// console.log(sqrdArr);
+
+// //Q5: reduce to calculate factorial of given number from an array of first n natural numbers
+// let arr = [1, 2, 3, 4, 5];
+// let factArr = arr.reduce((a, b) => {
+//   return a * b;
+// });
+// console.log(factArr);
