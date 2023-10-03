4906a13eacddd0d888fbe61288ecbb161f779f68
Psych <abdullahbinrafique945@gmail.com>
2023-10-03 13:07:35 +0500

    general things

--- Day_02/script.js
+++ Day_02/script.js
@@ -1,25 +1,28 @@
-console.log(console);
-console.error("This is error, 404");
-console.log("Agar sach likha ha to koi msla nahi wrna fail kr dy ga okay ye");
-console.assert(5 < 55); //it will give assertion failed
-obj = { a: 1, b: 2, c: 3 };
-console.table(obj);
-console.warn("shows warning: Hey don't drive when you're drunk");
-console.time("forLoop");
-for (let i = 0; i < 5; i++) {
-  console.log(i);
-}
-//this will give the time of the for loop
+// console.log(console);
+// console.error("This is error, 404");
+// console.log("Agar sach likha ha to koi msla nahi wrna fail kr dy ga okay ye");
+// console.assert(5 < 55); //it will give assertion failed
+// obj = { a: 1, b: 2, c: 3 };
+// console.table(obj);
+// console.warn("shows warning: Hey don't drive when you're drunk");
+// console.time("forLoop");
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+// //this will give the time of the for loop
 
-console.timeEnd("forLoop");
+// console.timeEnd("forLoop");
 
-// alert("Your script is actually working");
-let a = Number(prompt("Enter a value", "007"));
-alert("type of a is: " + typeof a);
-let writ = confirm("Do you want to write this on the page");
+// // alert("Your script is actually working");
+// let a = Number(prompt("Enter a value", "007"));
+// alert("type of a is: " + typeof a);
+// let writ = confirm("Do you want to write this on the page");
 
-if (writ) {
-  document.write(a);
-} else {
-  document.write("Please Allow me to write");
-}
+// if (writ) {
+//   document.write(a);
+// } else {
+//   document.write("Please Allow me to write");
+// }
+
+window.console.log(window);
+console.log(document);
---
+++ Day_03/quiz.js
@@ -0,0 +1,43 @@
+//Q1
+// let age = Number(prompt("Enter Your Age"));
+// // console.log(age);
+// const canDrive = (age) => {
+//   return age >= 18 ? true : false;
+// };
+
+// if (canDrive(age)) {
+//   alert("You can drive");
+// } else {
+//   alert("Your age is Less than 18, so you can't drive");
+// }
+
+//Q2
+
+// console.log(age);
+// const canDrive = (age) => {
+//   return age >= 18 ? true : false;
+// };
+// let runAgain = true;
+
+// while (runAgain) {
+//   let age = Number(prompt("Enter Your Age"));
+//   if (age < 0) {
+//     console.error("Enter age again");
+//     break;
+//   }
+//   if (canDrive(age)) {
+//     alert("You can drive");
+//   } else {
+//     alert("Your age is Less than 18, so you can't drive");
+//   }
+//   runAgain = confirm("Do you want to play again?");
+// }
+// // Q3
+// let num = Number(prompt("Enter a number"));
+// if (num > 4) {
+//   location.href = "https://google.com";
+// }
+
+// Q5.
+let color = prompt("Enter color for your background");
+document.body.style.backgroundColor = color;
