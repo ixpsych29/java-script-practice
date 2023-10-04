4e8126487194bceb8721c9b197de02e49e2a07cc
Psych <abdullahbinrafique945@gmail.com>
2023-10-04 19:54:25 +0500

    browser events

--- Day_04/practice.js
+++ Day_04/practice.js
@@ -1,7 +1,28 @@
 //<----------#practicing HTML attributes---------->
-let a = first.getAttribute("class");
-console.log(a);
-console.log(first.hasAttribute("class"));
-console.log(first.hasAttribute("div"));
-// first.setAttribute("hidden", "false");
-first.setAttribute("class", "nav nav-bar");
+// let a = first.getAttribute("class");
+// console.log(a);
+// console.log(first.hasAttribute("class"));
+// console.log(first.hasAttribute("div"));
+// // first.setAttribute("hidden", "false");
+// first.setAttribute("class", "nav nav-bar");
+// console.log(first.dataset);
+
+//<----------#Insertion Methods of HTMl---------->
+
+//<----------#ClassName && ClassList---------->
+
+//<----------#Set Timeout---------->    it means run the function after the given time once.
+// setTimeout(function () {
+//   document.write("Currently using TimeOut");
+// }, 5000);
+//5000 here is the miliseconds time
+
+//<----------#Set Interval----------> it means run again n again after every time given.
+// const add = (a, b) => {
+//   alert("add is running");
+//   console.log(a + b);
+//   return a + b;
+// };
+// setInterval(add, 5000, 5, 8);
+
+//<----------#Browser Events---------->
