778e0edf7584a201854c664953db79e4bc342edb
Psych <abdullahbinrafique945@gmail.com>
2023-10-05 11:59:09 +0500

    some minor modifications

---
+++ Exercises/Exercise_3.js
@@ -0,0 +1,8 @@
+//<----------#---------->
+//making a clock using js
+let a = new Date();
+let h = a.getHours();
+let m = a.getMinutes();
+let s = a.getSeconds();
+let d = a.getDate();
+console.log(h, m, s, d, a);
