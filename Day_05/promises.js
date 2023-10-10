ee1fc0b20d0bdba78f8118f65c2836f8265163ed
Psych <abdullahbinrafique945@gmail.com>
2023-10-10 13:23:35 +0500

    .then() and .catch()

--- Day_05/promises.js
+++ Day_05/promises.js
@@ -1,13 +1,35 @@
-//we'll practise here the promises, we use promises to make work parallelly
+//we'll practise here the promises, we use promises to make work parallelly and work asynchronously
 
-let promise = new Promise(function (resolve, reject) {
-  resolve("Promise is resolved");
+// let promise = new Promise(function (resolve, reject) {
+//   console.log("hello 4");
+//   resolve(7);
+// });
+
+// console.log("Hello 1");
+
+// setTimeout(() => {
+//   console.log("Hello 2");
+// }, 3000);
+
+// console.log("Hello 3");
+
+//promise methods: .then() and .catch()
+let p = new Promise(function (resolve, reject) {
+  console.log("Promise is Pending");
+  setTimeout(() => {
+    console.log("I'm Promise and is Rejected");
+    // resolve(true);
+    reject(new Error("Promise is Rejected"));
+  }, 5000);
 });
 
-console.log("Hello 1");
+// console.log(p);
+p.then(function (data) {
+  console.log(data);
+}).catch((error) => {
+  console.log(error);
+});
 
-setTimeout(() => {
-  console.log("Hello 2");
-}, 1000);
-
-console.log("Hello 3");
+// .catch(function (error) {
+//   console.log("error occured");
+//       });
