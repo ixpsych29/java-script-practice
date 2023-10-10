61bdf381221c47fb6ee13e7267b91e3e90b73d1e
Psych <abdullahbinrafique945@gmail.com>
2023-10-10 23:08:46 +0500

    promise methods

--- Day_05/promises.js
+++ Day_05/promises.js
@@ -57,27 +57,69 @@
 // });
 
 //<----------#---------->
-//quick quiz: load script function
-const loadscript = (src) => {
-  return new Promise((resolve, reject) => {
-    let script = document.createElement("script");
-    // script.type = "text/javascript";
-    script.src = src;
-    document.body.appendChild(script);
-    script.onload = () => {
-      resolve("the script is loaded successfully");
-    };
-    script.onerror = () => {
-      reject(0);
-    };
-  });
-};
+// //quick quiz: load script function
+// const loadscript = (src) => {
+//   return new Promise((resolve, reject) => {
+//     let script = document.createElement("script");
+//     // script.type = "text/javascript";
+//     script.src = src;
+//     document.body.appendChild(script);
+//     script.onload = () => {
+//       resolve("the script is loaded successfully");
+//     };
+//     script.onerror = () => {
+//       reject(0);
+//     };
+//   });
+// };
 
-let p1 = loadscript(
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-);
-p1.then((val) => {
+// let p1 = loadscript(
+//   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+// );
+// p1.then((val) => {
+//   console.log(val);
+// }).catch((error) => {
+//   console.log("we are having problem with script loading");
+// });
+
+//<----------#---------->
+//attaching multiple handlers (it's not promise chaining: we return the value first then use this value)
+
+//<----------#---------->
+//promise API: 6 primary methods to use this
+
+let p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("value 1");
+  }, 1000);
+});
+let p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error("Error Occurred"));
+  }, 2000);
+});
+let p3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("value 3");
+  }, 3000);
+});
+
+// p1.then((val) => {
+//   console.log(val);
+// });
+// p2.then((val) => {
+//   console.log(val);
+// });
+// p3.then((val) => {
+//   console.log(val);
+// });
+
+// /instead of using the above approach we use Promise.all() function
+// let promise_all = Promise.all([p1, p2, p3]).then((val) => {
+//   console.log(val);
+// });
+
+//Promise.allSettled(): it is used when one or more promise are rejected. it will fulfilled the promises and don't give errors
+let promise_all = Promise.allSettled([p1, p2, p3]).then((val) => {
   console.log(val);
-}).catch((error) => {
-  console.log("we are having problem with script loading");
 });
