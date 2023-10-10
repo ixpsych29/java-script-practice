65077a6d90e7da374782241dd10dbc4bdd621cd5
Psych <abdullahbinrafique945@gmail.com>
2023-10-10 22:32:39 +0500

    doing a small quiz

--- Day_05/promises.js
+++ Day_05/promises.js
@@ -14,22 +14,70 @@
 // console.log("Hello 3");
 
 //promise methods: .then() and .catch()
-let p = new Promise(function (resolve, reject) {
-  console.log("Promise is Pending");
-  setTimeout(() => {
-    console.log("I'm Promise and is Rejected");
-    // resolve(true);
-    reject(new Error("Promise is Rejected"));
-  }, 5000);
-});
+// let p = new Promise(function (resolve, reject) {
+//   console.log("Promise is Pending");
+//   setTimeout(() => {
+//     console.log("I'm Promise and is Rejected");
+//     // resolve(true);
+//     reject(new Error("Promise is Rejected"));
+//   }, 5000);
+// });
 
-// console.log(p);
-p.then(function (data) {
-  console.log(data);
-}).catch((error) => {
-  console.log(error);
-});
+// // console.log(p);
+// p.then(function (data) {
+//   console.log(data);
+// }).catch((error) => {
+//   console.log(error);
+// });
 
 // .catch(function (error) {
 //   console.log("error occured");
 //       });
+
+// /prommise chaining
+// let p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("Resolved p1 after 2 sec");
+//   }, 2000);
+//   resolve(56);
+// });
+
+// p1.then((val) => {
+//   console.log(val);
+//   let p2 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("p2 resolved!");
+//     }, 2000);
+//   });
+//   return p2;
+// }).then((val) => {
+//   console.log("we're done bruh with chaining");
+//   console.log(val);
+//   return 23;
+// });
+
+//<----------#---------->
+//quick quiz: load script function
+const loadscript = (src) => {
+  return new Promise((resolve, reject) => {
+    let script = document.createElement("script");
+    // script.type = "text/javascript";
+    script.src = src;
+    document.body.appendChild(script);
+    script.onload = () => {
+      resolve("the script is loaded successfully");
+    };
+    script.onerror = () => {
+      reject(0);
+    };
+  });
+};
+
+let p1 = loadscript(
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+);
+p1.then((val) => {
+  console.log(val);
+}).catch((error) => {
+  console.log("we are having problem with script loading");
+});
