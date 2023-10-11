8ed6016a812b6d41574f7618b82f408cf2904945
Psych <abdullahbinrafique945@gmail.com>
2023-10-11 12:37:02 +0500

    async_await

---
+++ Day_05/async_await.js
@@ -0,0 +1,3 @@
+//async funtion declare krty hain then hum parts of functions ko wait krwa skty hain
+//alternative of multiple .then()
+//async function always return a promise and non-promises in it
