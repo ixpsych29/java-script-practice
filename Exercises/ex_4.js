3fa2b4199cafafa5841d9fdd55edbeb265161cff
Psych <abdullahbinrafique945@gmail.com>
2023-10-15 21:32:41 +0500

    ex_4 done almost

--- Exercises/ex_4.js
+++ Exercises/ex_4.js
@@ -1,4 +1,14 @@
 setInterval(() => {
-  let dt = new Date();
-  time.innerHTML = dt;
+  let date = new Date();
+  let options = {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+
+  let d = date.toLocaleTimeString("en-us", options);
+  time.innerHTML = d;
 }, 1000);
