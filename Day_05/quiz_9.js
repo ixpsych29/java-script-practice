dad67bb1cb042bfc6be1eb667feeea3e661f0867
Psych <abdullahbinrafique945@gmail.com>
2023-10-15 19:17:20 +0500

    Q1 and Q2 done

--- Day_05/quiz_9.js
+++ Day_05/quiz_9.js
@@ -1,2 +1,28 @@
 //staring the quiz bruh
 // Q1:
+const loadscript = async (src) => {
+  return new Promise((resolve, reject) => {
+    let script = document.createElement("script");
+    script.src = src;
+    script.onload = () => {
+      resolve(src + " Done Success");
+    };
+    document.body.append(script);
+  });
+};
+
+// using .then()
+let a = loadscript("try_catch.js");
+a.then((val) => {
+  console.log(val);
+});
+
+//<-------------------->
+
+//Q2:
+// const funct = async () => {
+//   let a = await loadscript("try_catch.js");
+//   console.log(a);
+// };
+
+// funct();
