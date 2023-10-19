534b77b8396c94e1d4c649efd9b80dfb874fd678
Psych <abdullahbinrafique945@gmail.com>
2023-10-19 14:51:07 +0500

    basic OOPS in JS

--- Day_07/script.js
+++ Day_07/script.js
@@ -1,1 +1,33 @@
 // Starting OOPs in JavaScript
+class RailwayForm {
+  submit() {
+    alert(
+      this.name +
+        ", Your form is submitted and Your Train Number is: " +
+        this.trainNo
+    );
+  }
+  cancel() {
+    alert(
+      this.name +
+        ", Your form has cancelled and Your Train Number was: " +
+        this.trainNo
+    );
+  }
+  fillInfo(givenName, trainNo) {
+    this.name = givenName;
+    this.trainNo = trainNo;
+  }
+}
+
+let meForm = new RailwayForm();
+let psychForm = new RailwayForm();
+
+meForm.fillInfo("Abdullah Ibn Rafique", 112233);
+psychForm.fillInfo("PSYCH", 223344);
+
+meForm.submit();
+psychForm.submit();
+
+//but the moment "meForm" cancel the rerservation
+meForm.cancel();
