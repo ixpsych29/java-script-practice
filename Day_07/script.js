7fca7479c831df98596f6d4ab0f84c26053c0515
Psych <abdullahbinrafique945@gmail.com>
2023-10-19 15:08:32 +0500

    Constructor in JS

--- Day_07/script.js
+++ Day_07/script.js
@@ -1,5 +1,46 @@
-// Starting OOPs in JavaScript
+// // Starting OOPs in JavaScript
+// class RailwayForm {
+//   submit() {
+//     alert(
+//       this.name +
+//         ", Your form is submitted and Your Train Number is: " +
+//         this.trainNo
+//     );
+//   }
+//   cancel() {
+//     alert(
+//       this.name +
+//         ", Your form has cancelled and Your Train Number was: " +
+//         this.trainNo
+//     );
+//   }
+//   fillInfo(givenName, trainNo) {
+//     this.name = givenName;
+//     this.trainNo = trainNo;
+//   }
+// }
+
+// let meForm = new RailwayForm();
+// let psychForm = new RailwayForm();
+
+// meForm.fillInfo("Abdullah Ibn Rafique", 112233);
+// psychForm.fillInfo("PSYCH", 223344);
+
+// meForm.submit();
+// psychForm.submit();
+
+// //but the moment "meForm" cancel the rerservation
+// meForm.cancel();
+
+/* ************ Constructor in JS ************** */
+
 class RailwayForm {
+  constructor(givenName, trainNo) {
+    console.log("constrctor called");
+    this.name = givenName;
+    this.trainNo = trainNo;
+  }
+
   submit() {
     alert(
       this.name +
@@ -14,17 +55,17 @@ class RailwayForm {
         this.trainNo
     );
   }
-  fillInfo(givenName, trainNo) {
-    this.name = givenName;
-    this.trainNo = trainNo;
-  }
+  //   fillInfo(givenName, trainNo) {
+  //     this.name = givenName;
+  //     this.trainNo = trainNo;
+  //   }
 }
 
-let meForm = new RailwayForm();
-let psychForm = new RailwayForm();
+let meForm = new RailwayForm("Abdullah Ibn Rafique", 112233);
+let psychForm = new RailwayForm("Psych");
 
-meForm.fillInfo("Abdullah Ibn Rafique", 112233);
-psychForm.fillInfo("PSYCH", 223344);
+// meForm.fillInfo("Abdullah Ibn Rafique", 112233);
+// psychForm.fillInfo("PSYCH", 223344);
 
 meForm.submit();
 psychForm.submit();
