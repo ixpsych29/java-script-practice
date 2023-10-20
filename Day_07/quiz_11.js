315911fef18c1e0c3507b0424609c32999cd64cd
Psych <abdullahbinrafique945@gmail.com>
2023-10-20 12:37:43 +0500

    questions

--- Day_07/quiz_11.js
+++ Day_07/quiz_11.js
@@ -1,6 +1,6 @@
 /* *********** Chapter 11 Quiz ************ */
-//Q1:
-//Q2:
-//Q3:
-//Q4:
-//Q5:
+//Q1: Create A JavaScript class To create a complex number.Create a constructor to set the real and complex parts.
+//Q2: Write a method to add two complex numbers in the above class.
+//Q3: Create a class Student from a class Human. Override method and see changes.
+//Q4: See if Student is an instance of Human using instanceof keyword.
+//Q5: Use getters and setters to set and get the real and imaginary parts of the complex numbers.
