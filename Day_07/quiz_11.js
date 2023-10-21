a8b15d4b47d0eb894cd2cbb11d3142f00919056f
Psych <abdullahbinrafique945@gmail.com>
2023-10-21 15:59:05 +0500

    quiz_11 done AlHamduLillah

--- Day_07/quiz_11.js
+++ Day_07/quiz_11.js
@@ -2,28 +2,78 @@
 //Q1: Create A JavaScript class To create a complex number.Create a constructor to set the real and complex parts.
 // class complexNo {
 //   constructor(real, img) {
-//     console.log(`Complex number is ${real}+${img}i`);
+//     this.real = real;
+//     this.img = img;
+//     // console.log(`Complex number is ${real}+${img}i`);
 //   }
 // }
-// let real = prompt("Enter real part of complex number");
-// let img = prompt("Enter img part of complex number");
-// let cmplx = new complexNo(real, img);
+
+// let a = new complexNo(2, 4);
+// let b = new complexNo(7, 11);
 
 //Q2: Write a method to add two complex numbers in the above class.
-class complexNo {
-    constructor(real, img) {
-      console.log(`Complex number is ${real}+${img}i`);
-    }
-    add(){
-        let c1 = 
-    }
-  }
-  let real = prompt("Enter real part of complex number");
-  let img = prompt("Enter img part of complex number");
-  let cmplx = new complexNo(real, img);
+// class complexNo {
+//   constructor(real, img) {
+//     this.real = real;
+//     this.img = img;
+//   }
+//   add(numCmplx) {
+//     this.real = this.real + numCmplx.real;
+//     this.img = this.img + numCmplx.img;
+//   }
+// }
 
+// let a = new complexNo(2, 4);
+// let b = new complexNo(7, 11);
+// a.add(b);
+// console.log(`After Adding given two Complex Numbers: ${a.real}+${a.img}i`);
 
+//Q3: Create a class Student from a class Human. Override method and see changes.
+// class Human {
+//   constructor(name, favFood) {
+//     this.name = name;
+//     this.favFood = favFood;
+//   }
+//   walk() {
+//     console.log(`Human ${this.name} is walking!`);
+//   }
+// }
+// class Student extends Human {
+//   walk() {
+//     console.log(`Stduent ${this.name} is walking!`);
+//   }
+// }
 
-  //Q3: Create a class Student from a class Human. Override method and see changes.
+// let st = new Student("Psych", "Shimla Qeema");
+// st.walk();
+// let sy = new Human("Shapater", "Aloo Paratha");
+// sy.walk();
+
 //Q4: See if Student is an instance of Human using instanceof keyword.
+// console.log(st instanceof Human);   //true
+
 //Q5: Use getters and setters to set and get the real and imaginary parts of the complex numbers.
+class complexNo {
+  constructor(real, img) {
+    this.real = real;
+    this.img = img;
+    // console.log(`Complex number is ${real}+${img}i`);
+  }
+  getReal() {
+    return this.real;
+  }
+  getImg() {
+    return this.img;
+  }
+  setReal(real) {
+    this.real = real;
+  }
+  setImg(img) {
+    this.img = img;
+  }
+}
+
+let a = new complexNo(2, 4);
+let b = new complexNo(7, 11);
+console.log("real: " + a.getReal() + " & img: " + a.getImg());
+console.log("real: " + b.getReal() + " & img: " + b.getImg());
