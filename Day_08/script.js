55966cc107162d9f3c3b4f5d9fcd7a11af97dcf1
Psych <abdullahbinrafique945@gmail.com>
2023-10-23 15:39:16 +0500

    IIFE

---
+++ Day_08/script.js
@@ -0,0 +1,21 @@
+/* *********** Starting Chap 12 ************ */
+//IIFE: Immediately Invoked Function Expression
+//syntax: (function(){})(); whatever inside the () the brackets will call by the last 2 brackets.
+// let a = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(789);
+//     }, 3000);
+//   });
+// };
+
+// (async () => {
+//   let b = await a();
+//   console.log(b);
+//   let c = await a();
+//   console.log(c);
+//   let d = await a();
+//   console.log(d);
+// })();
+
+/* *********** Destructuring && Spread Operator ************ */
