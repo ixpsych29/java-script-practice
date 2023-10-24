52d6a53bdb8384c4ba41e0eb33497da5ee43bbfa
Psych <abdullahbinrafique945@gmail.com>
2023-10-24 15:26:20 +0500

    q2

--- Day_08/quiz_12.js
+++ Day_08/quiz_12.js
@@ -3,18 +3,26 @@
 //   console.log("Hello \n World");
 // }, 2000);
 
-const a = async (text) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(text);
-    }, 2000);
-  });
-};
+// const a = async (text) => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(text);
+//     }, 2000);
+//   });
+// };
 
-//using IIFE
-(async () => {
-  let text = await a("Hello");
-  console.log(text);
-  text = await a("World");
-  console.log(text);
-})();
+// //using IIFE
+// (async () => {
+//   let text = await a("Hello");
+//   console.log(text);
+//   text = await a("World");
+//   console.log(text);
+// })();
+
+//Q2:
+// const avg = (a, b, c) => {
+//   return (a + b + c) / 2;
+// };
+
+// let numArr = [1, 2, 3, 4, 5];
+// console.log(avg(...numArr));
