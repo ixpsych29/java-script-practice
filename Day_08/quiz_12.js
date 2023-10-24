b98e98975af788b5c566ae9315736f448f18a8af
Psych <abdullahbinrafique945@gmail.com>
2023-10-24 14:51:23 +0500

    q1

--- Day_08/quiz_12.js
+++ Day_08/quiz_12.js
@@ -1,4 +1,20 @@
 // Q1:
-setTimeout(() => {
-  console.log("Hello \n World");
-}, 2000);
+// setTimeout(() => {
+//   console.log("Hello \n World");
+// }, 2000);
+
+const a = async (text) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(text);
+    }, 2000);
+  });
+};
+
+//using IIFE
+(async () => {
+  let text = await a("Hello");
+  console.log(text);
+  text = await a("World");
+  console.log(text);
+})();
